4602dcbf45104ddd593c5ebde43a3ebd52399188
Okusa Robert <okusarobert@gmail.com>
2023-03-21 22:57:30 +0300

    query and path parameters

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
 from fastapi import FastAPI
-
+from typing import Optional
 app = FastAPI()
 
 @app.get('/')
 def index():
     return { 'data': { 'name': 'Okusa' } }
 
+@app.get('/blog')
+def index(limit=10, published:bool = True, sort: Optional[str] = None):
+    if published:
+        return { 'data': f'{limit} published blogs from db' }
+    else:
+        return { 'data': f'{limit} blogs from db' }
+
 @app.get('/about')
 def about():
     return { 'data': 'about page' }
+
+@app.get('/blog/unpublished')
+def unpublished():
+    
+    return { 'data': 'unpublished blogs' }
+
+@app.get('/blog/{id}')
+def show(id: int):
+    # fetch blog post with id = id
+    return { 'data': id }
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    # fetch comments from blog post where id = id
+    return { 'data': f'Comments from blog post {id}' }
+
