b55dac00483048101d87154525dc6292a7cf013e
Okusa Robert <okusarobert@gmail.com>
2023-03-22 21:37:34 +0300

    refactor code structure

--- blog/database.py
+++ blog/database.py
@@ -7,3 +7,12 @@ SQLALCHAMY_DATABASE_URL = 'sqlite:///./blog.db'
 engine = create_engine(SQLALCHAMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
---
+++ blog/routers/user.py
@@ -0,0 +1,28 @@
+from typing import List
+from fastapi import APIRouter, Depends, status, HTTPException
+from .. import schemas, models, database
+from sqlalchemy.orm import Session
+from ..hashing import Hash
+router = APIRouter(
+    prefix='/user',
+    tags=['Users']
+)
+
+
+@router.post('/', response_model=schemas.UserInfo)
+def create_user(request: schemas.User, db: Session = Depends(database.get_db)):
+    new_user = models.User(
+        name=request.name, email=request.email, password=Hash.bcrypt(request.password))
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
+@router.get('/{id}', response_model=schemas.UserInfo)
+def get_user(id: int, db: Session = Depends(database.get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'User with the id {id} is not available')
+    return user
