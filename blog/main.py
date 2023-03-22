232ac18d7d8a51c5434bbb74f43b1a27026b10eb
Okusa Robert <okusarobert@gmail.com>
2023-03-22 15:20:24 +0300

    read blogs, delete and update

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 from . import schemas, models
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
@@ -15,7 +15,7 @@ def get_db():
     finally:
         db.close()
 
-@app.post('/blog')
+@app.post('/blog', status_code=status.HTTP_201_CREATED)
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
@@ -23,13 +23,40 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id: int, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                        detail=f'Blog with id {id} was not found')
+    blog.delete(synchronize_session=False)
+    db.commit()
+    return 'deleted'
+
+
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id: int, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog with id {id} was not found')
+    blog.update(request.dict())
+    db.commit()
+    return 'updated'
+
 @app.get('/blog')
 def all(db: Session = Depends(get_db)):
     blogs  = db.query(models.Blog).all()
     return blogs
 
-@app.get('/blog/{id}')
-def show(id: int, db: Session = Depends(get_db)):
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK)
+def show(id: int, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Blog with the id {id} is not available')
+        # above is same as below
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {'details': f'Blog with the id {id} is not available'}
     return blog
     
